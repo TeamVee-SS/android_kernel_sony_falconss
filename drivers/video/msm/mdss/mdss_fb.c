240280d4bfda6990ef5401603c781cccaee841d6
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-11-19 19:59:51 -0200

    msm: mdss: Add lm3533 backlight code
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -53,6 +53,13 @@
 
 #include "mdss_fb.h"
 
+#ifdef CONFIG_LEDS_LM3533
+#include <linux/gpio.h>
+#include <linux/leds-lm3533.h>
+
+static int LCD_init = 0;
+#endif
+
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -857,6 +864,12 @@ static int mdss_fb_blank_sub(int blank_mode, struct fb_info *info,
 	switch (blank_mode) {
 	case FB_BLANK_UNBLANK:
 		if (!mfd->panel_power_on && mfd->mdp.on_fnc) {
+#ifdef CONFIG_LEDS_LM3533
+			if (LCD_init == 0) {
+				lm3533_backlight_control(0);
+				LCD_init = 1;
+			}
+#endif
 			ret = mfd->mdp.on_fnc(mfd);
 			if (ret == 0) {
 				mfd->panel_power_on = true;
@@ -1461,9 +1474,14 @@ static int mdss_fb_release_all(struct fb_info *info, bool release_all)
 
 	if (!mfd->ref_cnt) {
 		if (mfd->disp_thread) {
+#ifndef CONFIG_LEDS_LM3533
 			kthread_stop(mfd->disp_thread);
+#endif
 			mfd->disp_thread = NULL;
 		}
+#ifdef CONFIG_LEDS_LM3533
+		lm3533_backlight_control(0);
+#endif
 
 		ret = mdss_fb_blank_sub(FB_BLANK_POWERDOWN, info,
 			mfd->op_enable);
@@ -1803,6 +1821,12 @@ static int __mdss_fb_perform_commit(struct msm_fb_data_type *mfd)
 			pr_err("pan display failed %x on fb%d\n", ret,
 					mfd->index);
 	}
+#ifdef CONFIG_LEDS_LM3533
+	if (LCD_init == 0) {
+		lm3533_backlight_control(0);
+		LCD_init = 1;
+	}
+#endif
 	if (!ret)
 		mdss_fb_update_backlight(mfd);
 
