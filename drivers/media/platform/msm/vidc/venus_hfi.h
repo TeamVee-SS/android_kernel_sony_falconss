3f41d2e710502caace5a21d0b404c4c63eea25fa
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-12-22 13:06:08 -0200

    Revert "msm: vidc: unprepare clocks during power collapse"
    This reverts commit 2859d96956148052014cd2738c37ca22d95ee52f.

--- drivers/media/platform/msm/vidc/venus_hfi.h
+++ drivers/media/platform/msm/vidc/venus_hfi.h
@@ -124,12 +124,6 @@ enum bus_index {
 	BUS_IDX_MAX
 };
 
-enum clock_state {
-	DISABLED_UNPREPARED,
-	ENABLED_PREPARED,
-	DISABLED_PREPARED
-};
-
 struct vidc_mem_addr {
 	u8 *align_device_addr;
 	u8 *align_virtual_addr;
@@ -196,8 +190,8 @@ struct venus_hfi_device {
 	u32 clk_load;
 	u32 bus_load[MSM_VIDC_MAX_DEVICES];
 	unsigned long ocmem_size;
-	enum clock_state clk_state;
-	bool power_enabled;
+	u32 clocks_enabled;
+	u32 power_enabled;
 	enum vidc_clocks clk_gating_level;
 	struct mutex read_lock;
 	struct mutex write_lock;
